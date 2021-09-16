0c84adf983ff53eacfc0d6eb9c13b14bc5874634
Tokehos <88163971+Tokehos@users.noreply.github.com>
2021-09-16 10:05:59 +0300

    1

---
+++ src/main.java
@@ -0,0 +1,19 @@
+public class
+main {
+
+    public static void main(String[] args) {
+
+
+        int persent;
+        int balance = 100; // текущий баланс //
+        int enterMoney = 1990; // пополнение //
+        if (enterMoney < 1000) {
+            persent = 0;
+        } else {
+            persent = 1;
+        }
+        int bonus = enterMoney * persent / 100; // бонус //
+        int total = balance + enterMoney + bonus; // баланс с учетом пополнения //
+        System.out.println(total);
+    }
+}
